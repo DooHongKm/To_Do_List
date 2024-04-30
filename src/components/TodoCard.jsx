d6464df091597b7809e0ab198c25f563ec500793
DooHongKm <rlaenghdrlae@naver.com>
2024-04-30 10:52:34 +0900

    Added feature to read data

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
-export default function TodoCard() {
+export default function TodoCard({ title, time }) {
+  const h = time[0] < 10 ? '0' + time[0] : time[0]
+  const m = time[1] < 10 ? '0' + time[1] : time[1]
+  const s = time[2] < 10 ? '0' + time[2] : time[2]
   return (
     <div className='card-container'>
-      <h4>TodoCard</h4>
-      <p>Time</p>
+      <h4>{title}</h4>
+      <p>{h + ':' + m + ':' + s}</p>
       <div className='card-btn-container'>
         <button>수정</button>
         <button>삭제</button>
--- src/features/TodoList.jsx
+++ src/features/TodoList.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import TodoCard from '../components/TodoCard'
+import {data} from '../TodoData';
 
 export default function TodoList() {
   return (
     <div className='list-container'>
-      <TodoCard />
-      <TodoCard />
-      <TodoCard />
-      <TodoCard />
+      {data.todo.map((d) =>
+        <TodoCard
+          key={d.id}
+          title={d.title}
+          time={d.time}
+        />
+      )}
     </div>
   )
 }
